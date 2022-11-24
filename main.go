b1c6eb99d85bc080b868297f2813af50faff3048
@dewa.awidiya <dewa.awidiya@shopee.com>
2022-11-24 14:48:48 +0700

    feat: add cli example

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func promptInput(scanner *bufio.Scanner, text string) string {
+	fmt.Print(text)
+	scanner.Scan()
+	return scanner.Text()
+}
 
 func main() {
-	fmt.Println("hello world")
+	scanner := bufio.NewScanner(os.Stdin)
+	exit := false
+	menu := "Parking Lot\n" +
+		"1. Setup\n" +
+		"2. Park\n" +
+		"3. Un Park\n" +
+		"4. Exit"
+
+	for !exit {
+		fmt.Println(menu)
+		input := promptInput(scanner, "input menu: ")
+
+		switch input {
+		case "1":
+			capacities := promptInput(scanner, "input parking lot capacities: ")
+			fmt.Println("menu 1 selected: ", capacities)
+		case "4":
+			exit = true
+		default:
+			fmt.Println("invalid menu")
+		}
+	}
 }
